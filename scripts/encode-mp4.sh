cf5c67c686d5232e48ca014394f153168d2b1bf5
Pete Saia <iam@petesaia.com>
2020-07-26 11:37:00 -0400

    Add pete folk sit

--- scripts/encode-mp4.sh
+++ scripts/encode-mp4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ./scripts/encode-mp4.sh ./docs/raw/video.mp4 ./docs/mp4/video.mp4
-ffmpeg -i $1 -preset slow -tune film -y -filter:v "scale=-1:800" -vcodec h264 -crf 29 -an $2
+ffmpeg -i $1 -preset slow -tune film -y -filter:v "scale=-1:800" -vcodec h264 -crf 24 -an $2
